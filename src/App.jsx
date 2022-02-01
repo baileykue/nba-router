4831d9014d743c55c9cea3e5faf59eda7b355bdc
Bailey Kuebelbeck <bailey.kue@gmail.com>
2022-01-31 16:19:31 -0800

    set up basic files structure, starting to build routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
+import { BrowserRouter, NavLink } from 'react-router-dom';
+import Nav from './views/Nav/Nav';
+import Home from './views/Home/Home';
+
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Nav />
+        <Switch>
+          <Route>
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
 }
---
+++ src/views/Nav/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <div>
+      <NavLink to="/characters">Characters</NavLink>
+      {/* <NavLink to="/episodes">Episodes</NavLink>
+      <NavLink to="/storeNextDoor">Store Next Door</NavLink> */}
+    </div>
+  );
+}
